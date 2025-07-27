d2d4b26952a587f0ab6403332c78847a1fe0360a
manikumar-2375 <107806643+manikumar-2375@users.noreply.github.com>
2025-07-27 22:26:48 +0530

    Add files via upload

---
+++ house/Hall.java
@@ -0,0 +1,8 @@
+package house;
+import static java.lang.System.out;  // static import
+
+public class Hall {
+    public void showMessage() {
+        out.println("This is the first room while entering the house");
+    }
+}
